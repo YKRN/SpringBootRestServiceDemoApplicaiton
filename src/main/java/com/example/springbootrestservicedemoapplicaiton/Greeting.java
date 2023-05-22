56851c7d5ee00bfa6090c1728a9264a2fcad0d76
YKRN <yavuz@karan.net.tr>
2023-05-22 13:54:34 +0300

    Initial commit

---
+++ src/main/java/com/example/springbootrestservicedemoapplicaiton/Greeting.java
@@ -0,0 +1,8 @@
+package com.example.springbootrestservicedemoapplicaiton;
+
+
+    public record Greeting(long id, String content) {
+
+
+    }
+
---
+++ src/test/java/com/example/springbootrestservicedemoapplicaiton/SpringBootRestServiceDemoApplicaitonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestservicedemoapplicaiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceDemoApplicaitonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
